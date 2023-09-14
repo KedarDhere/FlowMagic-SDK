5339bc346998bdd250c85fdbd4c5ca6fabb242bb
KedarDhere <dherek@oregonstate.edu>
2023-09-13 19:16:33 -0700

    Add test case for addConnection function validation

--- Tests/FlowMagic-SDKTests/FlowMagic_SDKTests.swift
+++ Tests/FlowMagic-SDKTests/FlowMagic_SDKTests.swift
@@ -20,4 +20,51 @@ final class FlowMagicSDKTests: XCTestCase {
         XCTAssertEqual(expectedOutput[screenName]?.1, portNames,
                        "Screen's portNames are different than the input portnames")
     }
+
+    /// Tests the behavior of overwriting an existing screen registration and ensures port names remain unchanged.
+    func testOverwriteScreen() {
+        // Given
+        let mockScreenFlowProvider = ScreenFlowProvider()
+        let screenName = "Home"
+        let initialPortNames = ["Login", "SignUp"]
+        let overwrittenPortNames = ["Login", "SignUp", "AnotherPage"]
+        let view = AnyView(Home())
+
+        // Register the screen initially
+        mockScreenFlowProvider.registerScreen(screenName: screenName, portNames: initialPortNames, view: view)
+
+        // Try overwriting the screen
+        mockScreenFlowProvider.registerScreen(screenName: screenName, portNames: overwrittenPortNames, view: view)
+
+        // Then
+        let screenInfo = mockScreenFlowProvider.getScreens()[screenName]
+        XCTAssertNotNil(screenInfo)
+        XCTAssertEqual(screenInfo?.1, initialPortNames)
+    }
+
+    /// Tests the addition of a connection between screens through ports and validates the correct destination screen retrieval.
+    func testAddConnections() {
+        // Given
+        let mockScreenFlowProvider = ScreenFlowProvider()
+        let homeScreen = "Home"
+        let homeScrPortNames = ["Login", "SignUp"]
+        let home = AnyView(Home())
+
+        let signUpScreen = "SignUp"
+        let signUpScrPortNames = [String()]
+        let signUp = AnyView(SignUp())
+
+        let portName = "Home.SignUp"
+
+        // Register the screen initially
+        mockScreenFlowProvider.registerScreen(screenName: homeScreen, portNames: homeScrPortNames, view: home)
+        mockScreenFlowProvider.registerScreen(screenName: signUpScreen, portNames: signUpScrPortNames, view: signUp)
+
+        // When
+        mockScreenFlowProvider.addConnection(fromPort: "Home", toScreen: "SignUp")
+
+        // Then
+        let expectedOutput = mockScreenFlowProvider.getDestinationScreen(portName: portName)
+        XCTAssertNotNil(expectedOutput, "PortNames are not added as expected")
+    }
 }
